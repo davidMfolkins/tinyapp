3af8175fd3e8c2fa98505ff1c16b48d467650b8b
David Folkins <david.folkins88@gmail.com>
2020-09-29 19:44:04 -0400

    Added login page, and added links to registration and login pages to the header

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 
@@ -14,28 +14,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
-}
+};
 
 const emailLookUp = function(obj) {
-  let emails = []
-  for(const key in obj) {
-    for(const key2 in obj[key]) {
+  let emails = [];
+  for (const key in obj) {
+    for (const key2 in obj[key]) {
     }
-    emails.push(obj[key].email)
+    emails.push(obj[key].email);
   }
   return emails;
-}
+};
 
 
 app.get("/urls.json", (req, res) => {
@@ -43,7 +43,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
     user: users[req.cookies.user_id]
   };
@@ -53,13 +53,13 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     user: users[req.cookies.user_id]
-  }
+  };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
+  const templateVars = {
+    shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
     user: users[req.cookies.user_id]
   };
@@ -71,7 +71,11 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("urls_register")
+  res.render("urls_register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("urls_login");
 })
 
 app.post("/urls", (req, res) => {
@@ -91,39 +95,38 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("user_id", req.body.user)
-  res.redirect("/urls")
-})
+  res.send("OK")
+});
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id")
-  res.redirect("/register")
-})
+  res.clearCookie("user_id");
+  res.redirect("/login");
+});
 
 app.post("/register", (req, res) => {
   const randomID = Math.random().toString(36).substr(2, 6);
 
-  for(const email of emailLookUp(users)) {
+  for (const email of emailLookUp(users)) {
     if (req.body.email === email) {
-      res.send("Error 400: Email already exists.")
-      return
+      res.send("Error 400: Email already exists.");
+      return;
     }
   }
   if (req.body.password === "" || req.body.password === "") {
-    res.send("Error 400: Cannot leave email or password undefined")
-    return
+    res.send("Error 400: Cannot leave email or password undefined");
+    return;
   }
 
   users[randomID] = {
     id: randomID,
     email: req.body.email,
     password: req.body.password
-  }
-  console.log(req.body.email)
+  };
+  console.log(req.body.email);
 
-  res.cookie("user_id", randomID)
-  res.redirect("/urls")
-})
+  res.cookie("user_id", randomID);
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
