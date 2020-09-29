c827358ebfa164da2226956de534036fcb0828cd
David Folkins <david.folkins88@gmail.com>
2020-09-29 19:22:23 -0400

    added error handling for when user email already exists, or they leave email or password blank

--- express_server.js
+++ express_server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,6 +27,17 @@ const users = {
   }
 }
 
+const emailLookUp = function(obj) {
+  let emails = []
+  for(const key in obj) {
+    for(const key2 in obj[key]) {
+    }
+    emails.push(obj[key].email)
+  }
+  return emails;
+}
+
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -33,14 +45,14 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"] 
+    user: users[req.cookies.user_id]
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    user: users[req.cookies.user_id]
   }
   res.render("urls_new", templateVars);
 });
@@ -49,7 +61,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    user: users[req.cookies.user_id]
   };
   res.render("urls_show", templateVars);
 });
@@ -79,24 +91,37 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username)
+  res.cookie("user_id", req.body.user)
   res.redirect("/urls")
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
-  res.redirect("/urls")
+  res.clearCookie("user_id")
+  res.redirect("/register")
 })
 
 app.post("/register", (req, res) => {
   const randomID = Math.random().toString(36).substr(2, 6);
+
+  for(const email of emailLookUp(users)) {
+    if (req.body.email === email) {
+      res.send("Error 400: Email already exists.")
+      return
+    }
+  }
+  if (req.body.password === "" || req.body.password === "") {
+    res.send("Error 400: Cannot leave email or password undefined")
+    return
+  }
+
   users[randomID] = {
     id: randomID,
     email: req.body.email,
     password: req.body.password
   }
-  res.cookie("id", randomID)
-  console.log(users)
+  console.log(req.body.email)
+
+  res.cookie("user_id", randomID)
   res.redirect("/urls")
 })
 
