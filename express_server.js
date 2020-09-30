fd661c89731b8ac3c81e59b8d4f9c1aa075af2a3
David Folkins <david.folkins88@gmail.com>
2020-09-30 14:11:24 -0400

    completed the registration, login and logout feature of a user

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
@@ -27,14 +28,15 @@ const users = {
   }
 };
 
-const emailLookUp = function(obj) {
-  let emails = [];
+const userLookUp = function(obj, email) {
+  let user = [];
   for (const key in obj) {
-    for (const key2 in obj[key]) {
-    }
-    emails.push(obj[key].email);
+    user.push(obj[key]);
   }
-  return emails;
+  for (const item of user) {
+    if (item.email === email)
+      return item;
+  }
 };
 
 
@@ -71,12 +73,18 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("urls_register");
+  const templateVars = {
+    user: users[req.cookies.user_id]
+  };
+  res.render("urls_register", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  res.render("urls_login");
-})
+  const templateVars = {
+    user: users[req.cookies.user_id]
+  };
+  res.render("urls_login", templateVars);
+});
 
 app.post("/urls", (req, res) => {
   const randomID = Math.random().toString(36).substr(2, 6);
@@ -95,7 +103,15 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.send("OK")
+  const user = userLookUp(users, req.body.email);
+  if (user) {
+    if (user.password === req.body.password) {
+      res.cookie("user_id", user.id);
+      res.redirect("/urls");
+    }
+  } else {
+    res.send("403: Failed to login");
+  }
 });
 
 app.post("/logout", (req, res) => {
@@ -105,25 +121,20 @@ app.post("/logout", (req, res) => {
 
 app.post("/register", (req, res) => {
   const randomID = Math.random().toString(36).substr(2, 6);
-
-  for (const email of emailLookUp(users)) {
-    if (req.body.email === email) {
-      res.send("Error 400: Email already exists.");
-      return;
-    }
+  const user = userLookUp(users, req.body.email);
+  if (user) {
+    res.send("Error 400: Email already exists.");
+    return;
   }
-  if (req.body.password === "" || req.body.password === "") {
+  if (req.body.email === "" || req.body.password === "") {
     res.send("Error 400: Cannot leave email or password undefined");
     return;
   }
-
   users[randomID] = {
     id: randomID,
     email: req.body.email,
     password: req.body.password
   };
-  console.log(req.body.email);
-
   res.cookie("user_id", randomID);
   res.redirect("/urls");
 });
